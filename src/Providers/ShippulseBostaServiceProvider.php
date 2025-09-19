fe347178d30a564e3ab463883791c8f34fa3a37b
Karim Mohamed <komicho1996@gmail.com>
2025-09-19 15:37:17 +0300

    first commit

---
+++ src/Providers/ShippulseBostaServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Obelaw\Shippulse\Bosta\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ShippulseBostaServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Boot logic for the service provider
+    }
+}
